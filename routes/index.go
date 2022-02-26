c385ce7bb40f124458606b9c83c741611d70edee
Ariel Costas <ariel@costas.dev>
2022-02-26 01:34:13 +0100

    show actual time, not all struct

--- routes/index.go
+++ routes/index.go
@@ -1,23 +1,24 @@
 package routes
 
 import (
+	"log"
 	"net/http"
 )
-
-type Resp struct {
-	Now string
-}
 
 type IndexParams struct {
 	Row string
 }
 
 func IndexPage(w http.ResponseWriter, r *http.Request) {
-	row := db.QueryRow("SELECT curtime()")
-	var resp Resp = Resp{}
-	row.Scan(&resp.Now)
+	row := db.QueryRow("SELECT curtime() as Now")
+	dbresp := struct{ Now string }{}
+	err := row.Scan(&dbresp.Now)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	println(dbresp.Now)
 
 	t.ExecuteTemplate(w, "index.html", &IndexParams{
-		Row: resp.Now,
+		Row: dbresp.Now,
 	})
 }
