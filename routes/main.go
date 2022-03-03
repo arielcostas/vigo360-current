75c0e150add914c81d40b198d4acca70ea770c08
Ariel Costas <ariel@costas.dev>
2022-03-03 15:12:39 +0100

    log request referer, will be used for stats in the future

--- routes/main.go
+++ routes/main.go
@@ -48,7 +48,12 @@ func FullCanonica(path string) string {
 
 func TestMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("[%s] %s %s\n", time.Now().Format("15:04:05"), r.Method, r.RequestURI)
+		fmt.Printf("[%s] %s %s", time.Now().Format("15:04:05"), r.Method, r.RequestURI)
+		if !strings.HasSuffix(r.Referer(), r.RequestURI) {
+			fmt.Printf(" - Ref: %s\n", r.Referer())
+		} else {
+			fmt.Print("\n")
+		}
 		next.ServeHTTP(w, r)
 	})
 }
