63563428953cb6d37a15fb604fcef58498792af3
Ariel Costas <ariel@costas.dev>
2022-03-05 19:23:26 +0100

    admin: implement almost all login

--- routes/admin.go
+++ routes/admin.go
@@ -1,14 +1,24 @@
 package routes
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
 	_ "embed"
+
+	"github.com/thanhpk/randstr"
 )
 
-type AdminLoginParams struct{}
+type AdminLoginParams struct {
+	LoginError  bool
+	PrefillName string
+}
+
+func StartSession() {
+
+}
 
 func AdminLogin(w http.ResponseWriter, r *http.Request) {
 	// Serve the form and end
@@ -24,19 +34,40 @@ func AdminLogin(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	var row struct {
-		id       string
-		nombre   string
-		password string
+		Id         string
+		Nombre     string
+		Contraseña string
 	}
-	err := db.QueryRow("SELECT id, nombre, contraseña FROM autores WHERE id=?;", r.PostFormValue("userid")).Scan(&row.id, &row.nombre, &row.password)
-	if err != nil {
-		println(err.Error())
+	err := db.QueryRowx("SELECT id, nombre, contraseña FROM autores WHERE id=?;", r.PostFormValue("userid")).StructScan(&row)
+	if err == sql.ErrNoRows {
+		//TODO log failed login
+		t.ExecuteTemplate(w, "admin-login.html", &AdminLoginParams{
+			PrefillName: r.PostFormValue("userid"),
+			LoginError:  true,
+		})
 	}
 
-	pass := ValidatePassword(r.PostFormValue("password"), row.password)
-	fmt.Fprintf(w, `id => %s
-nombre => %s
-contraseña => %t
-	`, row.id, row.nombre, pass)
+	pass := ValidatePassword(r.PostFormValue("password"), row.Contraseña)
 
+	if !pass {
+		t.ExecuteTemplate(w, "admin-login.html", &AdminLoginParams{
+			PrefillName: r.PostFormValue("userid"),
+			LoginError:  true,
+		})
+		return
+	}
+
+	token := randstr.String(20)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "sess",
+		Value:    token,
+		Path:     "/admin",
+		Domain:   r.URL.Host,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+		Secure:   true,
+	})
+
+	fmt.Fprintf(w, "id => %s\nnombre => %s\ncontraseña => %t\n", row.Id, row.Nombre, pass)
 }
