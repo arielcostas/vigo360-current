e85a8469ceccbe178a90b76d4ee6de879eb59456
Ariel Costas <ariel@costas.dev>
2022-03-19 15:27:23 +0100

    Convert migrations into 'origin' schema

--- _schema/00-base.sql
+++ _schema/00-base.sql
@@ -1,14 +1,7 @@
--- 
--- WARNING: THIS SCRIPT WIPES THE DATABASE WITH ALL THE DATA
--- 
-DROP SCHEMA IF EXISTS vigo360;
-CREATE SCHEMA vigo360;
-USE vigo360;
+CREATE SCHEMA IF NOT EXISTS vigo360_2;
+USE vigo360_2;
 
---
--- COMIENZA LA CREACION DE TABLAS
---
-CREATE TABLE autores(
+CREATE TABLE IF NOT EXISTS autores(
 	id varchar(40) NOT NULL,
     nombre varchar(40) NOT NULL,
     email varchar(150) NOT NULL UNIQUE,
@@ -21,33 +14,29 @@ CREATE TABLE autores(
 	PRIMARY KEY (id)
 );
 
--- 
--- Rama publicaciones
--- 
-CREATE TABLE publicaciones(
+CREATE TABLE IF NOT EXISTS publicaciones(
 	id varchar(40) NOT NULL,
     fecha_publicacion datetime DEFAULT NULL,
     fecha_actualizacion datetime DEFAULT NOW() ON UPDATE NOW(),
-    alt_portada varchar(200) NOT NULL,
+    alt_portada varchar(300) NOT NULL,
     
     titulo varchar(80) NOT NULL,
-    resumen varchar(200) NOT NULL,
+    resumen varchar(300) NOT NULL,
     contenido text NOT NULL,
 
     autor_id varchar(40) NOT NULL,
+    serie_id varchar(40) DEFAULT NULL,
+    serie_posicion int DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE palabras_clave(
+CREATE TABLE IF NOT EXISTS tags(
 	id int NOT NULL AUTO_INCREMENT,
     nombre varchar(40) NOT NULL,
     PRIMARY KEY (id)
 );
 
--- 
--- Rama vehiculos
--- 
-CREATE TABLE fotografias(
+CREATE TABLE IF NOT EXISTS fotografias(
 	id int NOT NULL AUTO_INCREMENT,
     titulo varchar(80) NOT NULL,
     descripcion varchar(500) NOT NULL,
@@ -62,14 +51,14 @@ CREATE TABLE fotografias(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE licencias(
+CREATE TABLE IF NOT EXISTS licencias(
 	id varchar(10) NOT NULL,
     titulo varchar(50) NOT NULL,
     url varchar(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE vehiculos(
+CREATE TABLE IF NOT EXISTS vehiculos(
 	id char(10) NOT NULL,
     matricula char(10) NOT NULL UNIQUE,
     numeracion_empresa varchar(8) NOT NULL,
@@ -79,7 +68,7 @@ CREATE TABLE vehiculos(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE tipos_vehiculo(
+CREATE TABLE IF NOT EXISTS tipos_vehiculo(
 	id int NOT NULL AUTO_INCREMENT,
 	fabricante varchar(80) NOT NULL,
     modelo varchar(80) NOT NULL,
@@ -91,7 +80,7 @@ CREATE TABLE tipos_vehiculo(
     PRIMARY KEY(id)
 );
 
-CREATE TABLE empresas(
+CREATE TABLE IF NOT EXISTS empresas(
 	id int NOT NULL AUTO_INCREMENT,
     nombre varchar(80) NOT NULL,
     descripcion varchar(2000) NOT NULL,
@@ -100,15 +89,12 @@ CREATE TABLE empresas(
     PRIMARY KEY (id)
 );
 
---
--- Rama trabajos
---
-CREATE TABLE trabajos(
+CREATE TABLE IF NOT EXISTS trabajos(
 	id varchar(40) NOT NULL,
     titulo varchar(80) NOT NULL,
-    resumen varchar(200) NOT NULL,
+    resumen varchar(300) NOT NULL,
     contenido text NOT NULL,
-    alt_portada varchar(200) NOT NULL,
+    alt_portada varchar(300) NOT NULL,
     fecha_publicacion datetime NOT NULL DEFAULT NOW(),
     fecha_actualizacion datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     
@@ -116,15 +102,16 @@ CREATE TABLE trabajos(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE adjuntos(
-	id int NOT NULL,
+CREATE TABLE IF NOT EXISTS adjuntos(
+	id int NOT NULL AUTO_INCREMENT,
     nombre_archivo varchar(50) NOT NULL UNIQUE,
+    titulo varchar(80) NOT NULL,
     fecha_subida datetime NOT NULL DEFAULT NOW(),
     trabajo_id varchar(40) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE sesiones(
+CREATE TABLE IF NOT EXISTS sesiones(
     sessid char(20) NOT NULL,
     iniciada datetime NOT NULL DEFAULT NOW(),
     revocada boolean DEFAULT false,
@@ -133,21 +120,30 @@ CREATE TABLE sesiones(
     PRIMARY KEY (sessid)
 );
 
---
--- FOREIGN KEYS
---
+CREATE TABLE IF NOT EXISTS avisos(
+    id int NOT NULL AUTO_INCREMENT,
+    fecha_creacion datetime DEFAULT NOW(),
+    titulo varchar(100) NOT NULL,
+    contenido varchar(1000) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS series(
+    id varchar(40) NOT NULL,
+    titulo varchar(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS publicaciones_tags (
+	publicacion_id varchar(40) NOT NULL,
+    tag_id int NOT NULL,
+    PRIMARY KEY (publicacion_id, tag_id),
+    FOREIGN KEY ppc_publicacion (publicacion_id) REFERENCES publicaciones(id),
+	FOREIGN KEY ppc_palabraclave(tag_id) REFERENCES tags(id)
+);
 
 -- autor redacta publicacion
 ALTER TABLE publicaciones ADD FOREIGN KEY publicaciones_autor(autor_id) REFERENCES autores(id);
-
--- publicacion pertenece a palabras clave
-CREATE TABLE publicaciones_palabrasclave (
-	publicacion_id varchar(40) NOT NULL,
-    palabraclave_id int NOT NULL,
-    PRIMARY KEY (publicacion_id, palabraclave_id),
-    FOREIGN KEY ppc_publicacion (publicacion_id) REFERENCES publicaciones(id),
-	FOREIGN KEY ppc_palabraclave(palabraclave_id) REFERENCES palabras_clave(id)
-);
 
 -- autor comparte fotografia
 ALTER TABLE fotografias ADD FOREIGN KEY fotografias_autor(autor_id) REFERENCES autores(id);
@@ -164,43 +160,15 @@ ALTER TABLE vehiculos ADD FOREIGN KEY vehiculos_tipovehiculo(tipo_vehiculo_id) R
 -- tipo_vehiculo propiedad de empresa
 ALTER TABLE tipos_vehiculo ADD FOREIGN KEY tiposvehiculo_empresa(empresa_id) REFERENCES empresas(id);
 
--- autores publican trabajos
-CREATE TABLE autores_trabajos (
-	autor_id varchar(40) NOT NULL,
-    trabajo_id varchar(40) NOT NULL,
-    PRIMARY KEY (autor_id, trabajo_id),
-    FOREIGN KEY autorestrabajos_autor(autor_id) REFERENCES autores(id),
-    FOREIGN KEY autorestrabajos_trabajo(trabajo_id) REFERENCES trabajos(id)
-);
-
 -- trabajo contiene adjuntos
 ALTER TABLE adjuntos ADD FOREIGN KEY adjuntos_trabajo(trabajo_id) REFERENCES trabajos(id);
 
 -- autor inicia sesión
 ALTER TABLE sesiones ADD FOREIGN KEY sesiones_autor(autor_id) REFERENCES autores(id);
 
-/*
- *   MIGRACION 1 - Adjuntos
- */
-ALTER TABLE adjuntos MODIFY COLUMN id int NOT NULL AUTO_INCREMENT;
-ALTER TABLE adjuntos ADD COLUMN titulo varchar(80) NOT NULL;
+-- publicación pertenece (opcionalmente) a serie
+ALTER TABLE publicaciones ADD FOREIGN KEY publicaciones_series(serie_id) REFERENCES series(id);
 
-/*
- *   MIGRACION 2 - Palabras clave -> tags
- */
-RENAME TABLE palabras_clave TO tags;
-RENAME TABLE publicaciones_palabrasclave TO publicaciones_tags;
-ALTER TABLE publicaciones_tags RENAME COLUMN palabraclave_id TO tag_id;
-
-/*
- *   MIGRACION 3 - Texto alternativo más largo
- */
-ALTER TABLE publicaciones MODIFY COLUMN alt_portada varchar(300) NOT NULL,
-	MODIFY COLUMN resumen varchar(300) NOT NULL;
-    
-/*
- *   MIGRACION 4 - Vista para Sitemap
- */
 CREATE VIEW sitemap AS SELECT uri, DATE(fecha_actualizacion) as fecha_actualizacion, priority, changefreq FROM (
 	SELECT CONCAT("/tags/", publicaciones_tags.tag_id) as uri, fecha_actualizacion, "0.3" as priority, "weekly" as changefreq FROM publicaciones
 		RIGHT JOIN publicaciones_tags ON publicaciones.id = publicaciones_tags.publicacion_id
@@ -219,27 +187,3 @@ CREATE VIEW sitemap AS SELECT uri, DATE(fecha_actualizacion) as fecha_actualizac
 	UNION
 	(SELECT "/" as uri, fecha_actualizacion, "0.9" as priority, "weekly" as changefreq FROM publicaciones ORDER BY fecha_actualizacion DESC LIMIT 1)
 ) as pq;
-
-/*
- *  MIGRACION 5 - Avisos para administradores
- */
-CREATE TABLE avisos(
-    id int NOT NULL AUTO_INCREMENT,
-    fecha_creacion datetime DEFAULT NOW(),
-    titulo varchar(100) NOT NULL,
-    contenido varchar(1000) NOT NULL,
-    PRIMARY KEY(id)
-);
-
-/*
- *  MIGRACION 6 - Series de artículos
- */
-CREATE TABLE series(
-    id varchar(40) NOT NULL,
-    titulo varchar(50) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE publicaciones ADD COLUMN serie_id varchar(40) DEFAULT NULL;
-ALTER TABLE publicaciones ADD COLUMN serie_posicion int DEFAULT NULL;
-ALTER TABLE publicaciones ADD FOREIGN KEY publicaciones_series(serie_id) REFERENCES series(id);
