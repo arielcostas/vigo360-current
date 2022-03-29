52355c7efa6900fc0b9a55917a100b76d3129cf8
Ariel Costas <ariel@costas.dev>
2022-03-30 00:49:22 +0200

    Implementar una gestión de errores centralizada
    De este modo, evitamos repetir el mismo código (función de error, logger y
    devolver) en todos los sitios donde puede haber error.
    
    Sigue esta guía: https://go.dev/blog/error-handling-and-go

--- admin/main.go
+++ admin/main.go
@@ -75,7 +75,7 @@ func InitRouter() *mux.Router {
 
 	router.HandleFunc("/admin/dashboard", DashboardPage).Methods("GET")
 	router.HandleFunc("/admin/post", PostListPage).Methods("GET")
-	router.HandleFunc("/admin/post", CreatePostAction).Methods("POST")
+	router.Handle("/admin/post", appHandler(createPost)).Methods("POST")
 
 	router.HandleFunc("/admin/post/{id}", EditPostPage).Methods("GET")
 	router.HandleFunc("/admin/post/{id}", EditPostAction).Methods("POST")
--- admin/post.go
+++ admin/post.go
@@ -48,104 +48,62 @@ func PostListPage(w http.ResponseWriter, r *http.Request) {
 }
 
 // Data to be input via a form to create a new post
-type CreatePostActionFormInput struct {
-	Id     string `validate:"required,min=3,max=40,lowercase"`
-	Titulo string `validate:"required,min=3,max=80"`
-}
+func createPost(w http.ResponseWriter, r *http.Request) *appError {
+	art_autor := verifyLogin(w, r).Id
 
-// HTTP Handler for creating posts, accessible by authenticated authors via `POST /admin/post`. It requires passing art-id and art-titulo in the body, as part of the form submission from the `GET` page with the same URI.
-func CreatePostAction(w http.ResponseWriter, r *http.Request) {
-	sesion := verifyLogin(w, r)
-	err := r.ParseForm()
-
-	fi := CreatePostActionFormInput{}
-
-	if err != nil {
-		logger.Error("error parsing create-post form: %s", err.Error())
-		InternalServerErrorHandler(w, r)
-		return
+	if err := r.ParseForm(); err != nil {
+		return &appError{Error: err, Message: "error parsing form",
+			Response: "Hubo un error leyendo el formulario", Status: 500}
 	}
 
+	fi := struct {
+		Id     string `validate:"required,min=3,max=40,lowercase"`
+		Titulo string `validate:"required,min=3,max=80"`
+	}{}
 	fi.Id = r.FormValue("art-id")
 	fi.Titulo = r.FormValue("art-titulo")
 
-	err = validator.New().Struct(fi)
-	if err != nil {
-		logger.Error("[post] validation error: %s", err.Error())
-		w.WriteHeader(400)
-		w.Write([]byte("Error de validación"))
-		return
+	if err := validator.New().Struct(fi); err != nil {
+		// TODO: Mostrar de nuevo formulario con los errores
+		return &appError{Error: err, Message: "error de validacion",
+			Response: "Ha habido un error validando el formulario. Revise los campos e inténtelo de nuevo.", Status: 400}
 	}
 
-	art_autor := sesion.Id
-
-	tx, err := db.Begin()
-	if err != nil {
-		w.WriteHeader(500)
-		logger.Error("[post] error beginning insert operation: %s", err.Error())
-		w.Write([]byte("Error creando el artículo"))
-		err2 := tx.Rollback()
-		if err2 != nil {
-			logger.Error("[post] error reverting database: %s", err2.Error())
-		}
-		return
+	var tx *sql.Tx
+	if nt, err := db.Begin(); err != nil {
+		return &appError{Error: err, Message: "error beginning transaction",
+			Response: "Error creando publicación en la de datos", Status: 500}
+	} else {
+		tx = nt
 	}
 
-	_, err = tx.Exec(`INSERT INTO publicaciones(id, titulo, alt_portada, resumen, contenido, autor_id) VALUES (?, ?, "CAMBIAME","", "", ?);`, fi.Id, fi.Titulo, art_autor)
-
-	if err != nil {
-		// TODO: proper error page
-		w.WriteHeader(500)
-		logger.Error("[post] error creating article in database: %s", err.Error())
-		w.Write([]byte("Error creando el artículo"))
-		err2 := tx.Rollback()
-		if err2 != nil {
-			logger.Error("[post] error reverting database: %s", err2.Error())
-		}
-		return
+	q := "INSERT INTO publicaciones(id, titulo, alt_portada, resumen, contenido, autor_id) VALUES (?, ?, 'CAMBIAME','', '', ?);"
+	if _, err := tx.Exec(q, fi.Id, fi.Titulo, art_autor); err != nil {
+		tx.Rollback()
+		return &appError{Error: err, Message: "error creating article in database",
+			Response: "Error creando publicación en la de datos", Status: 500}
 	}
 
-	// Every article needs its default photo
 	photopath := os.Getenv("UPLOAD_PATH")
-	err = os.WriteFile(photopath+"/images/"+fi.Id+".webp", defaultImageWebp, 0o644)
-	if err != nil {
-		// TODO: proper error page
-		w.WriteHeader(500)
-		logger.Error("[post] error saving article webp: %s", err.Error())
-		w.Write([]byte("Error creando foto WEBP predeterminada"))
-		err2 := tx.Rollback()
-		if err2 != nil {
-			logger.Error("[post] error reverting database: %s", err2.Error())
-		}
-		return
-	}
-	err = os.WriteFile(photopath+"/thumb/"+fi.Id+".jpg", defaultImageJPG, 0o644)
-	if err != nil {
-		// TODO: proper error page
-		w.WriteHeader(500)
-		logger.Error("[post] error saving article jpg: %s", err.Error())
-		w.Write([]byte("Error creando foto JPG predeterminada"))
-		err2 := tx.Rollback()
-		if err2 != nil {
-			logger.Error("[post] error reverting database: %s", err2.Error())
-		}
-		return
+	if err := os.WriteFile(photopath+"/images/"+fi.Id+".webp", defaultImageWebp, 0o644); err != nil {
+		tx.Rollback()
+		return &appError{Error: err, Message: "error saving webp to disk",
+			Response: "Hubo un error guardando el artículo", Status: 500}
 	}
 
-	err = tx.Commit()
+	if err := os.WriteFile(photopath+"/thumb/"+fi.Id+".jpg", defaultImageWebp, 0o644); err != nil {
+		tx.Rollback()
+		return &appError{Error: err, Message: "error saving jpg to disk",
+			Response: "Hubo un error guardando el artículo", Status: 500}
+	}
 
-	if err != nil {
-		// TODO: proper error page
-		w.WriteHeader(500)
-		logger.Error("[post] error commiting article in database: %s", err.Error())
-		w.Write([]byte("Error creando el artículo"))
-		err2 := tx.Rollback()
-		if err2 != nil {
-			logger.Error("[post] error reverting database: %s", err2.Error())
-		}
-		return
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return &appError{Error: err, Message: "error performing transaction",
+			Response: "Error creando publicación en la de datos", Status: 500}
 	}
 
 	w.Header().Add("Location", "/admin/post/"+fi.Id)
 	w.WriteHeader(303)
+	return nil
 }
