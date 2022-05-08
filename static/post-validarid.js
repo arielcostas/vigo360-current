d8a5c9e76cfc4c1f0c1c6cb97657f7d5d2a40d22
Ariel Costas <arielcostas@mailbox.org>
2022-05-08 15:56:07 +0200

    Eliminar log sobrante

--- static/post-validarid.js
+++ static/post-validarid.js
@@ -1,5 +1,4 @@
 var artIDfield = document.getElementById("art-id")
 artIDfield.addEventListener("keyup", e => {
 	e.target.value = e.target.value.replaceAll(" ", "-").toLowerCase()
-	console.log(e.target.value)
 })
