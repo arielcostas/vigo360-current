1302ee5fe69960a47d48d363812613850d74deed
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-06-22 11:08:54 +0000

    Arreglar borrado de fotos extra

--- internal/handleAdminDeleteFotoExtra.go
+++ internal/handleAdminDeleteFotoExtra.go
@@ -5,16 +5,13 @@ import (
 	"net/http"
 	"os"
 
-	"vigo360.es/new/internal/database"
 	"vigo360.es/new/internal/logger"
 	"vigo360.es/new/internal/messages"
-	"vigo360.es/new/internal/models"
 )
 
 func (s *Server) handleAdminDeleteFotoExtra() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger.NewLogger(r.Context().Value(ridContextKey("rid")).(string))
-		sess, _ := r.Context().Value(sessionContextKey("sess")).(models.Session)
 
 		uploadPath := os.Getenv("UPLOAD_PATH")
 
@@ -22,32 +19,6 @@ func (s *Server) handleAdminDeleteFotoExtra() http.HandlerFunc {
 		if fotoId == "" {
 			logger.Error("falta el parámetro foto en la URL")
 			s.handleJsonError(w, 500, messages.ErrorFormulario)
-			return
-		}
-
-		/*
-			Comprobar que el usuario que intenta eliminar la fotografía sea el autor, y que la publicación no esté pública
-		*/
-		row, err := database.GetDB().Query(`SELECT COALESCE(fecha_publicacion, ""), autor_id FROM publicaciones WHERE id = ?`)
-		if err != nil {
-			logger.Error("error recuperando publicación cuya foto se va a borrar: %s", err.Error())
-			s.handleJsonError(w, 500, messages.ErrorDatos)
-			return
-		}
-
-		var dbFechaPub, dbAutorId string
-		row.Scan(&dbFechaPub, &dbAutorId)
-
-		if dbFechaPub != "" {
-			logger.Error("la publicación ya es pública, impidiendo borrado")
-			s.handleJsonError(w, 400, "No se puede eliminar fotografías de una publicación pública")
-			return
-		}
-
-		// TODO: Crear permiso para saltarse esta limitación
-		if sess.Autor_id != dbAutorId {
-			logger.Error("el usuario no es el autor, impidiendo borrado")
-			s.handleJsonError(w, 400, messages.ErrorSinPermiso)
 			return
 		}
 
