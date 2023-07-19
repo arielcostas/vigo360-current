554d0941b0484346af2d4284613fab28c6e96b60
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-07-19 19:03:05 +0200

    Página de error mejorada y nuevas tipografías

--- internal/handleError.go
+++ internal/handleError.go
@@ -3,15 +3,32 @@ package internal
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"vigo360.es/new/internal/templates"
 
 	"vigo360.es/new/internal/messages"
 )
+
+type errorResponse struct {
+	Rid          string
+	ErrorCode    int
+	Message      string
+	RequestedUrl string
+	Time         string
+}
 
 func (s *Server) handleError(r *http.Request, w http.ResponseWriter, status int, message messages.ErrorMessage) {
 	w.WriteHeader(status)
 	rid := r.Context().Value(ridContextKey("rid")).(string)
 
-	_, err := fmt.Fprintf(w, `<!DOCTYPE html><html><body><h1>%s</h1></body></html>`, message)
+	err := templates.Render(w, "_error.html", &errorResponse{
+		Rid:          rid,
+		ErrorCode:    status,
+		Message:      fmt.Sprintf("%s", message),
+		RequestedUrl: r.URL.String(),
+		Time:         time.Now().Format("2006-01-02 15:04:05"),
+	})
+
 	if err != nil {
 		_, _ = fmt.Fprintf(w, `{ "error": "%s" }`, messages.ErrorFatal)
 		fmt.Println(rid, err)
