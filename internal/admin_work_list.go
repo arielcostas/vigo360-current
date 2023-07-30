dd6a4cbdae52f8f1a1ddfc7ac4ee38640de47229
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-07-30 12:37:01 +0200

    Basic work list

--- internal/admin_work_list.go
+++ internal/admin_work_list.go
@@ -14,20 +14,19 @@ import (
 )
 
 // TODO: Get rid of this
-type ResumenPost struct {
+type ResumenTrabajo struct {
 	Id                string
 	Titulo            string
 	Fecha_publicacion string
-	CantTags          int
 	Publicado         bool
 	Autor_id          string
 	Autor_nombre      string
 }
 
-func (s *Server) handleAdminListPost() http.HandlerFunc {
-	const MAX_LISTED_POSTS = 50
+func (s *Server) handleAdminListTrabajos() http.HandlerFunc {
+	const MAX_LISTED_WORKS = 50
 	type response struct {
-		Posts   []ResumenPost
+		Works   []ResumenTrabajo
 		Session models.Session
 	}
 
@@ -35,28 +34,37 @@ func (s *Server) handleAdminListPost() http.HandlerFunc {
 		log := logger.NewLogger(r.Context().Value(ridContextKey("rid")).(string))
 		sess := r.Context().Value(sessionContextKey("sess")).(models.Session)
 		db := database.GetDB()
-		posts := []ResumenPost{}
+		trabajos := []ResumenTrabajo{}
 
-		err := db.Select(&posts, `SELECT publicaciones.id, titulo, (fecha_publicacion < NOW() && fecha_publicacion IS NOT NULL) as publicado, COALESCE(fecha_publicacion, "") as fecha_publicacion, autor_id, autores.nombre as autor_nombre, count(tag_id) as canttags FROM publicaciones LEFT JOIN autores ON publicaciones.autor_id = autores.id LEFT JOIN publicaciones_tags ON publicaciones.id = publicaciones_tags.publicacion_id GROUP BY publicaciones.id ORDER BY publicado ASC, publicaciones.fecha_publicacion DESC;`)
+		err := db.Select(&trabajos, `SELECT trabajos.id,
+       titulo,
+       (fecha_publicacion < NOW() && fecha_publicacion IS NOT NULL) as publicado,
+       COALESCE(fecha_publicacion, "")                              as fecha_publicacion,
+       autor_id,
+       autores.nombre                                               as autor_nombre
+FROM trabajos
+         LEFT JOIN autores ON trabajos.autor_id = autores.id
+GROUP BY trabajos.id
+ORDER BY publicado, trabajos.fecha_publicacion DESC;`)
 
 		if err != nil && !errors.Is(err, sql.ErrNoRows) {
-			log.Error("error recuperando listado de publicacionoes: %s", err.Error())
+			log.Error("error recuperando listado de trabajos: %s", err.Error())
 			s.handleError(r, w, 500, messages.ErrorDatos)
 			return
 		}
 
-		for i, p := range posts {
+		for i, p := range trabajos {
 			tiempo, _ := time.Parse("2006-01-02 15:04:05", p.Fecha_publicacion)
 			p.Fecha_publicacion = tiempo.Format("02/01/2006")
-			posts[i] = p
+			trabajos[i] = p
 		}
 
-		if len(posts) > MAX_LISTED_POSTS {
-			posts = posts[:MAX_LISTED_POSTS]
+		if len(trabajos) > MAX_LISTED_WORKS {
+			trabajos = trabajos[:MAX_LISTED_WORKS]
 		}
 
-		err = templates.Render(w, "admin-post.html", &response{
-			Posts:   posts,
+		err = templates.Render(w, "admin-works.html", &response{
+			Works:   trabajos,
 			Session: sess,
 		})
 
