061b5c393c26e91acd48112f321c5fdf4238373d
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-10-29 22:08:41 +0100

    Include EasyMDE locally

---
+++ static/editor-mde.js
@@ -0,0 +1,8 @@
+let mde = new EasyMDE({
+	element: document.querySelector("[data-mde]"),
+	autoDownloadFontAwesome: false,
+	lineNumbers: true,
+	maxHeight: "500px",
+	spellChecker: false,
+	tabSize: 4
+});
